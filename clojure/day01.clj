07c6c9ee5619d158da434f271a24a0c73b77b066
Michael Sims <michael.h.sims@gmail.com>
2025-01-16 00:13:06 -0600

    Final day 1 tweaks
    - Simplify solve-part-two based on feedback from Clojurians Slack and
      Discord
    - Remove the comments. They were there to assist in post-merge code
      review

--- clojure/day01.clj
+++ clojure/day01.clj
@@ -1,37 +1,18 @@
 (ns day01
-  "https://adventofcode.com/2015/day/1"
-  (:require
-    [clojure.string :as str]))
+  "https://adventofcode.com/2015/day/1")
 
-(defn get-input-list []
+(def input
   (slurp "inputs/input1.txt"))
 
+(def directions
+  (map {\( 1 \) -1} input))
+
 (defn solve-part-one []
-  "Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the
-  directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the
-  instructions one character at a time.
-
-  An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down
-  one floor.
-
-  The apartment building is very tall, and the basement is very deep; he will never find the top or bottom floors.
-
-  [examples omitted]
-
-  To what floor do the instructions take Santa?"
-  (let [directions (map {\( 1 \) -1} (get-input-list))]
-    (apply + directions)))
+  (apply + directions))
 
 (defn solve-part-two []
-  "Now, given the same instructions, find the position of the first character that causes him to enter the basement
-  (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
-
-  [examples omitted]
-
-  What is the position of the character that causes Santa to first enter the basement?"
   (->>
-    (get-input-list)
-    (map {\( 1 \) -1})
+    directions
     (reductions +)
     (take-while (complement neg?))
     count
